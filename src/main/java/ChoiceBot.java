100958369491901852bab58ba9048aad0e9f1bbc
amptea <martin.tan31@gmail.com>
2025-08-20 14:39:17 +0800

    Implement add and list
    Texxt entered by user is stored in an ArrayList
    and displayed back to them when requested.

--- src/main/java/ChoiceBot.java
+++ src/main/java/ChoiceBot.java
@@ -1,3 +1,7 @@
+import java.awt.desktop.SystemSleepEvent;
+import java.lang.management.BufferPoolMXBean;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ChoiceBot {
@@ -5,16 +9,26 @@ public class ChoiceBot {
         Scanner sc = new Scanner(System.in);
         final String BOT_NAME = "ChoiceBot";
         final String USER_NAME = "User";
-        System.out.println(BOT_NAME + ": Hello, Welcome to Choice!");
+        ArrayList<String> taskList = new ArrayList<>();
+
+        System.out.println(BOT_NAME + ": Hello, Welcome to ChoiceBot!");
         System.out.println(BOT_NAME + ": What would you like to do?");
         System.out.println("---------------------------");
         while (true) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if (command.equals("bye")) {
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println("\t" + (i + 1) + ". " + taskList.get(i));
+                    System.out.println("---------------------------");
+                }
+            } else {
+                taskList.add(command);
+                System.out.println("\tadded: " + command);
+                System.out.println("---------------------------");
             }
-            System.out.println(BOT_NAME + ": " + command);
         }
-        System.out.println("Thanks for stopping by! See you again!");
+        System.out.println(BOT_NAME + ": Thanks for stopping by! See you again!");
     }
 }
