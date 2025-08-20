fdaa3a7ee5be421476c880b30f6a4471a8572bf1
amptea <martin.tan31@gmail.com>
2025-08-20 14:25:58 +0800

    Add bot identifier to message
    Add bot identifier so it is easier to see who is
    sending each message.

--- src/main/java/ChoiceBot.java
+++ src/main/java/ChoiceBot.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 public class ChoiceBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello, Welcome to Choice!");
-        System.out.println("What would you like to do?");
+        final String BOT_NAME = "ChoiceBot";
+        final String USER_NAME = "User";
+        System.out.println(BOT_NAME + ": Hello, Welcome to Choice!");
+        System.out.println(BOT_NAME + ": What would you like to do?");
         System.out.println("---------------------------");
         while (true) {
             String command = sc.next();
             if (command.equals("bye")) {
                 break;
             }
-            System.out.println(command);
+            System.out.println(BOT_NAME + ": " + command);
         }
         System.out.println("Thanks for stopping by! See you again!");
     }
