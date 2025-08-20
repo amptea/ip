58085db19ec54d2349caea55b46a9b1d438ce3f9
amptea <martin.tan31@gmail.com>
2025-08-20 14:18:24 +0800

    Implement echo feature
    Echo commands entered by user, and exit when user
    types bye.

--- src/main/java/Choice.java
+++ src/main/java/Choice.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Choice {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello, Welcome to Choice!");
         System.out.println("What would you like to do?");
         System.out.println("---------------------------");
+        while (true) {
+            String command = sc.next();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(command);
+        }
         System.out.println("Thanks for stopping by! See you again!");
     }
 }
